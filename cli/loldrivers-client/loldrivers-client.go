9397d3608aa616d97577cd8e4a9ef6ea0894002f
rtfmkiesel <79413747+rtfmkiesel@users.noreply.github.com>
2023-05-14 23:56:05 +0200

    refactored; moved code into modules reduced the usage of go funcs

--- cli/loldrivers-client/loldrivers-client.go
+++ cli/loldrivers-client/loldrivers-client.go
@@ -5,6 +5,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"sync"
 	"time"
 
@@ -41,7 +42,7 @@ Modes:
   internal  Use the built-in driver set (can be outdated, fallback)
 
 Options:
-  -d        Directory to scan for drivers (default: Windows Default Driver Folders)
+  -d        Directory to scan for drivers (default: Windows default driver folders)
             Files which cannot be opened or read will be silently ignored
   -l        Size limit for files to scan in MB (default: 10)
             Be aware, higher values greatly increase runtime & CPU usage
@@ -63,108 +64,27 @@ Options:
 	logger.Banner()
 	logger.Log("[*] Started")
 
-	// To store the drivers in
-	var drivers []loldrivers.Driver
-
-	// Load the drivers based on the selected mode
-	logger.Verbose(fmt.Sprintf("[*] Mode '%s'", flagMode))
-
-	switch flagMode {
-	case "online":
-		// Default, download from the web
-		// Download drivers
-		var err error
-		drivers, err = loldrivers.Get()
-		if err != nil {
-			// There was a parsing error
-			logger.Catch(err)
-			logger.Log("[!] Got an error while parsing online data. Falling back to internal data set")
-			drivers, err = loldrivers.Parse(loldrivers.InternalDrivers)
-			if err != nil {
-				logger.CatchCrit(err)
-			}
-		}
-
-	case "local":
-		// User wants to use a local file
-		if flagLocalFile == "" {
-			logger.CatchCrit(fmt.Errorf("mode 'local' requires '-f'"))
-		}
-
-		// Read file
-		jsonBytes, err := filesystem.FileRead(flagLocalFile)
-		if err != nil {
-			logger.CatchCrit(err)
-		}
-
-		// Parse file
-		drivers, err = loldrivers.Parse(jsonBytes)
-		if err != nil {
-			// There was a parsing error
-			logger.Catch(err)
-			logger.Log("[!] Got an error while parsing local file. Falling back to internal data set")
-			drivers, err = loldrivers.Parse(loldrivers.InternalDrivers)
-			if err != nil {
-				logger.CatchCrit(err)
-			}
-		}
-
-	case "internal":
-		// User wants to use internal data set
-		// Parse bytes
-		var err error
-		drivers, err = loldrivers.Parse(loldrivers.InternalDrivers)
-		if err != nil {
-			logger.CatchCrit(err)
-		}
-
-	default:
-		logger.CatchCrit(fmt.Errorf("invalid mode '%s'", flagMode))
+	// Load the drivers
+	drivers, err := loldrivers.LoadDrivers(flagMode, flagLocalFile)
+	if err != nil {
+		logger.CatchCrit(err)
 	}
-
 	logger.Verbose(fmt.Sprintf("[*] Loaded %d drivers", len(drivers)))
 
-	// To store all driver checksums
-	driverHashes := loldrivers.DriverHashes{}
-	// Get all checksums from the loaded drivers
-	for _, driver := range drivers {
-		for _, knownVulnSample := range driver.KnownVulnerableSamples {
-			// Append MD5 if exist
-			if knownVulnSample.MD5 != "" && knownVulnSample.MD5 != "-" {
-				driverHashes.MD5Sums = append(driverHashes.MD5Sums, knownVulnSample.MD5)
-			}
-			// Append SHA1 if exist
-			if knownVulnSample.SHA1 != "" && knownVulnSample.SHA1 != "-" {
-				driverHashes.SHA1Sums = append(driverHashes.SHA1Sums, knownVulnSample.SHA1)
-			}
-			// Append SHA256 if exist
-			if knownVulnSample.SHA256 != "" && knownVulnSample.SHA256 != "-" {
-				driverHashes.SHA256Sums = append(driverHashes.SHA256Sums, knownVulnSample.SHA256)
-			}
-		}
-	}
-
+	// Get all hashes from the loaded drivers
+	driverHashes := loldrivers.GetHashes(drivers)
 	logger.Verbose(fmt.Sprintf("[*] Got %d MD5 hashes", len(driverHashes.MD5Sums)))
 	logger.Verbose(fmt.Sprintf("[*] Got %d SHA1 hashes", len(driverHashes.SHA1Sums)))
 	logger.Verbose(fmt.Sprintf("[*] Got %d SHA256 hashes", len(driverHashes.SHA256Sums)))
 
 	// Create the channels and waitgroup for the checksum runners
 	chanFiles := make(chan string)
-	chanChecksums := make(chan checksums.Sums)
-	wgChecksums := new(sync.WaitGroup)
+	chanResults := make(chan logger.Result)
+	wgRunner := new(sync.WaitGroup)
 	// Spawn the checksum runners
 	for i := 0; i <= flagThreads; i++ {
-		go checksums.CalcRunner(wgChecksums, chanFiles, chanChecksums)
-		wgChecksums.Add(1)
-	}
-
-	// Create the channels and waitgroup for the compare runners
-	chanResults := make(chan logger.Result)
-	wgCompare := new(sync.WaitGroup)
-	// Spawn the compare runners
-	for i := 0; i <= flagThreads; i++ {
-		go checksums.CompareRunner(wgCompare, chanChecksums, chanResults, driverHashes)
-		wgCompare.Add(1)
+		go checksums.Runner(wgRunner, chanFiles, chanResults, driverHashes)
+		wgRunner.Add(1)
 	}
 
 	// Create the waitgroup for the output runner
@@ -173,55 +93,33 @@ Options:
 	go logger.OutputRunner(wgOutput, chanResults)
 	wgOutput.Add(1)
 
-	// Scan for drivers
-	//
-	// A tiny amount of time could be saved if only ".sys" files were checked.
-	// I tried and the time saved is not worth it since malicious drivers could have other file extensions.
+	// Set the folders which are going to be scanned for files
+	var paths []string
 	if flagDir == "" {
-		// User did not specify a path, use the default Windows paths
-
 		// Since scanning the default folders requires admin privileges, check here
-		if !filesystem.IsAdmin() {
+		if _, err := os.Open("\\\\.\\PHYSICALDRIVE0"); err != nil {
 			logger.CatchCrit(fmt.Errorf("not running with administrative privileges"))
 		}
 
-		for _, path := range filesystem.DriverPaths {
-			// Get all files
-			files, err := filesystem.FilesInFolder(path, flagFileLimit)
-			if err != nil {
-				logger.CatchCrit(err)
-			}
-
-			// Add jobs to the channel
-			for _, file := range files {
-				chanFiles <- file
-			}
-		}
+		// User did not specify a path with '-d', use the default Windows paths
+		paths = append(paths, "C:\\Windows\\System32\\drivers")
+		paths = append(paths, "C:\\Windows\\System32\\DriverStore\\FileRepository")
 	} else {
-		// Scan the user specified folder for drivers
-		// Get all files
-		files, err := filesystem.FilesInFolder(flagDir, flagFileLimit)
-		if err != nil {
-			logger.CatchCrit(err)
-		}
+		// User specified a custom folder to scan
+		paths = append(paths, flagDir)
+	}
 
-		// Add jobs to the channel
-		for _, file := range files {
-			chanFiles <- file
+	// Get all files from subfolders and send them to the checksum runners via a channel
+	for _, path := range paths {
+		if err := filesystem.FileWalker(path, flagFileLimit, chanFiles); err != nil {
+			logger.CatchCrit(err)
 		}
 	}
 
 	// Close the channel to start the checksum runners
 	close(chanFiles)
-	// Wait here until all checksums are calculated
-	logger.Verbose("[*] Waiting for the checksum runners to complete")
-	wgChecksums.Wait()
-
-	// Close the channel to start the compare runners
-	close(chanChecksums)
-	// Wait here until all compares have been done
-	logger.Verbose("[*] Waiting for the compare runners to complete")
-	wgCompare.Wait()
+	// Wait here until all checksums are calculated and compared
+	wgRunner.Wait()
 
 	// Close the results channel to process the results
 	close(chanResults)
